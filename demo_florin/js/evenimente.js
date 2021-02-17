2f9ccb661808bf25552b731cc4acfc17955529f5
Florin Catalin <23093563+florincatalin@users.noreply.github.com>
2021-02-17 19:09:34 +0200

    Create evenimente.js

---
+++ demo_florin/js/evenimente.js
@@ -0,0 +1,62 @@
+myEvents = [
+  { 
+    id: "01",
+    name: "Colegiul „Ferdinand“", 
+	badge: "ora: 10.30",
+    date: "Fri Feb 17 2021 00:00:00 GMT+0200 (Eastern European Standard Time)", 
+	description: "clasa a-IX-a G, prof. Turcu Alina",
+    type: "vizita", 
+    everyYear: false, 
+	color: "#008c23"
+  },
+  { 
+    id: "02",
+    name: "Colegiul „Antipa“", 
+	badge: "ora: 11.30",
+    date: "Fri Feb 17 2021 00:00:00 GMT+0200 (Eastern European Standard Time) ", 
+	description: "clasa a-X-a B, prof. Cojan George",
+    type: "workshop", 
+    everyYear: false,
+    color: "#0000ff"
+  },
+  { 
+    id: "03",
+    name: "Colegiul „Vrânceanu“", 
+	badge: "ora: 12.30",
+    date: "Fri Feb 17 2021 00:00:00 GMT+0200 (Eastern European Standard Time)", 
+	description: "clasa a-XII-a A, prof. Ambrozie Adriana",
+    type: "vizita", 
+    everyYear: false, 
+	color: "#008c23"
+  },
+  {
+    id: "04",
+    name: "Școala „Alecu Russo“", 
+	badge: "ora: 13.30",
+    date: "Fri Feb 17 2021 00:00:00 GMT+0200 (Eastern European Standard Time)", 
+	description: "clasa a-VI-a C, prof. Neagu Gheorghe",
+    type: "multimedia", 
+    everyYear: false, 
+	color: "#ffff00"
+  },
+    { 
+    id: "05",
+    name: "Şcoala „Alexandru Platon“", 
+	badge: "ora: 14.00",
+    date: "Fri Feb 17 2021 00:00:00 GMT+0200 (Eastern European Standard Time) ", 
+	description: "clasa a-4-a C, prof. Stelian Aurica",
+    type: "workshop", 
+    everyYear: false,
+    color: "#0000ff"
+  },
+  { 
+    id: "06",
+    name: "Colegiul „D. Mangeron“", 
+	badge: "ora: 14.00",
+    date: "Fri Feb 17 2021 00:00:00 GMT+0200 (Eastern European Standard Time) ", 
+	description: "clasa a-4-a C, prof. Panţâru Lucian",
+    type: "vizită", 
+    everyYear: false,
+    color: "#008c23"
+  }
+];
